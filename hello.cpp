4ee0dd7961091ab5cb4a92ec04a1b2b75b3e60cf
lostwin <smile_184@hotmail.com>
2024-09-07 18:05:51 -0500

    iniciando

---
+++ hello.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define ll long long int
+
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define vi vector<int>
+#define vll vector<ll>
+#define vct vector
+#define pb push_back
+#define er erase
+#define fi(i,a,n,w) for(int i =a;i<n;i+=w)
+#define fl(i,a,n,w) for(ll i=a;i<n;i+=w)
+#define fr first
+#define se second
+#define sz(a) a.size()
+#define mstr(r) for(auto i:r)cout<<i<<" ";
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define ingr(r) fi(i,0,sz(r),1)cin>>r[i];
+
+using namespace std;
+
+void solve(){
+
+cout<<1;
+
+}
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+freopen("/home/lostwin/C++ code/Problemas/input.txt","r",stdin);
+freopen("/home/lostwin/C++ code/Problemas/output.txt","w",stdout);
+#endif
+fast
+	int t; cin>>t;
+	while(t--){
+		solve();
+	}
+
+return 0;
+}
