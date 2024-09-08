bd4a1a753f75bc98c0a2c8f3f592824342c9186b
chalinx <smile_184@hotmail.com>
2024-09-07 20:02:30 -0500

    Update hello.cpp
    adios no

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define ll long long int
-
+//adiosno
 #define all(a) a.begin(),a.end()
 #define fast ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 #define vi vector<int>
